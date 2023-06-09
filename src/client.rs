a643177336218c3f3d7ae955f863fc6005e0e492
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-06-09 10:31:54 +0100

    Start moving to WebDriver-based system

--- src/client.rs
+++ src/client.rs
@@ -1,67 +1,96 @@
 use std::collections::HashMap;
 
-use reqwest::{
-    header::{HeaderMap, InvalidHeaderValue, COOKIE},
-    Client, ClientBuilder, Url,
+use fantoccini::{
+    error::{CmdError, NewSessionError},
+    Client, ClientBuilder, Locator,
 };
 use thiserror::Error;
-use url::ParseError;
+use url::Url;
 
-use crate::member::{Member, StudentId};
+use crate::{
+    client,
+    member::{Member, StudentId},
+};
+
+const BASE_URL: &str = "https://su.nottingham.ac.uk";
 
 #[derive(Debug, Error)]
 pub enum SumsClientNewError {
-    #[error("Failed to parse base URL.")]
-    BaseUrlParseError(#[from] ParseError),
+    // #[error("Failed to parse base URL.")]
+    // BaseUrlParseError(#[from] ParseError),
 
-    #[error("Failed to create default cookie header. Is su_session in the valid header format?")]
-    CookieHeaderParseError(#[from] InvalidHeaderValue),
-
-    #[error("Failed to initialise client.")]
-    ClientBuildError(#[from] reqwest::Error),
+    // #[error("Failed to create default cookie header. Is su_session in the valid header format?")]
+    // CookieHeaderParseError(#[from] InvalidHeaderValue),
+    // #[error("Failed to initialise client.")]
+    // ClientBuildError(#[from] reqwest::Error),
+    #[error("Failed to create new WebDriver session")]
+    WebDriverNewSessionError(#[from] NewSessionError),
 }
 
 #[derive(Debug, Error)]
 pub enum SumsClientError {
-    #[error("Failed to join base URL.")]
-    BaseUrlJoinError(#[from] ParseError),
+    // #[error("Failed to join base URL.")]
+    // BaseUrlJoinError(#[from] ParseError),
 
-    #[error("Failed to make request.")]
-    RequestError(#[from] reqwest::Error),
+    // #[error("Failed to make request.")]
+    // RequestError(#[from] reqwest::Error),
+    #[error("A WebDriver command failed")]
+    WebDriverCmdError(#[from] CmdError),
 }
 
 pub struct SumsClient {
-    base_url: Url,
     client: Client,
     group_id: u16,
-    su_session: String,
 }
 
 impl SumsClient {
-    pub fn new(group_id: u16, su_session: String) -> Result<Self, SumsClientNewError> {
-        let base_url = Url::parse(&format!(
-            "https://student-dashboard.sums.su/groups/{}",
-            group_id
-        ))?;
+    pub async fn new<S>(group_id: u16, webdriver_address: S) -> Result<Self, SumsClientNewError>
+    where
+        S: AsRef<str>,
+    {
+        let client = ClientBuilder::rustls()
+            .connect(webdriver_address.as_ref())
+            .await?;
 
-        let mut headers = HeaderMap::new();
-        headers.insert(COOKIE, format!("su_session={}", su_session).parse()?);
-
-        let client = ClientBuilder::new().default_headers(headers).build()?;
-
-        Ok(SumsClient {
-            base_url,
-            client: client,
-            group_id,
-            su_session,
-        })
+        Ok(Self { client, group_id })
     }
 
-    pub async fn get_members(&self) -> Result<HashMap<StudentId, Member>, SumsClientError> {
-        let url = self.base_url.join("members")?;
+    pub async fn authenticate<S>(&self, username: S, password: S) -> Result<(), SumsClientError>
+    where
+        S: AsRef<str>,
+    {
+        self.client.goto(BASE_URL).await?;
 
-        let response = self.client.get(url).send().await?;
+        self.client
+            .find(Locator::LinkText("Student Login"))
+            .await?
+            .click()
+            .await?;
 
-        Ok(HashMap::new())
+        println!("{}", self.client.current_url().await?);
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::client::{SumsClient, SumsClientNewError};
+
+    const GROUP_ID: u16 = 213;
+    const WEBDRIVER_ADDRESS: &str = "http://localhost:4444";
+
+    #[tokio::test]
+    async fn test_create_client() -> Result<(), SumsClientNewError> {
+        let client = SumsClient::new(GROUP_ID, WEBDRIVER_ADDRESS).await?;
+
+        assert_eq!(GROUP_ID, client.group_id);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_auth() {
+        // let client = SumsClient::new(GROUP_ID, WEBDRIVER_ADDRESS).await?;
     }
 }
