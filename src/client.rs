a2658726021688ea6d4cf84aec004928b8810d62
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-06-09 12:14:20 +0100

    WebDriver auth working

--- src/client.rs
+++ src/client.rs
@@ -15,27 +15,30 @@ use crate::{
 const BASE_URL: &str = "https://su.nottingham.ac.uk";
 
 #[derive(Debug, Error)]
-pub enum SumsClientNewError {
-    // #[error("Failed to parse base URL.")]
-    // BaseUrlParseError(#[from] ParseError),
+pub enum SumsClientError {
+    #[error("A WebDriver command failed")]
+    WebDriverCmdError(#[from] CmdError),
+}
 
-    // #[error("Failed to create default cookie header. Is su_session in the valid header format?")]
-    // CookieHeaderParseError(#[from] InvalidHeaderValue),
-    // #[error("Failed to initialise client.")]
-    // ClientBuildError(#[from] reqwest::Error),
+#[derive(Debug, Error)]
+pub enum SumsClientNewError {
     #[error("Failed to create new WebDriver session")]
     WebDriverNewSessionError(#[from] NewSessionError),
 }
 
 #[derive(Debug, Error)]
-pub enum SumsClientError {
-    // #[error("Failed to join base URL.")]
-    // BaseUrlJoinError(#[from] ParseError),
+pub enum SumsClientAuthError {
+    #[error("A generic error occured (details within SumsClientError)")]
+    SumsClientError(#[from] SumsClientError),
 
-    // #[error("Failed to make request.")]
-    // RequestError(#[from] reqwest::Error),
-    #[error("A WebDriver command failed")]
-    WebDriverCmdError(#[from] CmdError),
+    #[error("Authentication failed with message {0}")]
+    AuthFailedError(String),
+}
+
+impl From<CmdError> for SumsClientAuthError {
+    fn from(err: CmdError) -> Self {
+        SumsClientAuthError::SumsClientError(SumsClientError::WebDriverCmdError(err))
+    }
 }
 
 pub struct SumsClient {
@@ -55,30 +58,64 @@ impl SumsClient {
         Ok(Self { client, group_id })
     }
 
-    pub async fn authenticate<S>(&self, username: S, password: S) -> Result<(), SumsClientError>
+    pub async fn authenticate<S>(&self, username: S, password: S) -> Result<(), SumsClientAuthError>
     where
         S: AsRef<str>,
     {
         self.client.goto(BASE_URL).await?;
 
+        // Click on the user icon in the top right
         self.client
-            .find(Locator::LinkText("Student Login"))
+            .find(Locator::Id("userActionsInvoker"))
             .await?
             .click()
             .await?;
 
-        println!("{}", self.client.current_url().await?);
+        // Click on the student login button
+        self.client
+            .find(Locator::XPath("//*[@id=\"userActions\"]/ul/li[1]/a[1]"))
+            .await?
+            .click()
+            .await?;
 
-        Ok(())
+        // Find the UoN login form
+        let login_form = self
+            .client
+            .form(Locator::XPath("/html/body/div/div/div/div[1]/form"))
+            .await?;
+
+        // Fill in the username/password
+        login_form
+            .set(Locator::Id("username"), username.as_ref())
+            .await?;
+        login_form
+            .set(Locator::Id("password"), password.as_ref())
+            .await?;
+
+        login_form.submit().await?;
+
+        let login_error = self
+            .client
+            .find(Locator::XPath("/html/body/div/div/div/div[1]/section/p"))
+            .await;
+
+        match login_error {
+            Ok(element) => Err(SumsClientAuthError::AuthFailedError(element.text().await?)),
+            Err(_) => Ok(()),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::client::{SumsClient, SumsClientNewError};
+    use std::env;
+
+    use crate::client::{SumsClient, SumsClientAuthError, SumsClientNewError};
+
+    use super::SumsClientError;
 
     const GROUP_ID: u16 = 213;
-    const WEBDRIVER_ADDRESS: &str = "http://localhost:4444";
+    const WEBDRIVER_ADDRESS: &str = "http://localhost:9515";
 
     #[tokio::test]
     async fn test_create_client() -> Result<(), SumsClientNewError> {
@@ -90,7 +127,15 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_auth() {
-        // let client = SumsClient::new(GROUP_ID, WEBDRIVER_ADDRESS).await?;
+    async fn test_auth() -> Result<(), SumsClientAuthError> {
+        // test_create_client should handle this
+        let client = SumsClient::new(GROUP_ID, WEBDRIVER_ADDRESS).await.unwrap();
+
+        let username = env::var("SUMS_USERNAME").expect("Invalid username environment variable");
+        let password = env::var("SUMS_PASSWORD").expect("Invalid password environment variable");
+
+        client.authenticate(username, password).await?;
+
+        Ok(())
     }
 }
